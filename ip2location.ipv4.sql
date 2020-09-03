8e7e9a867346ae7fc5a66e71f1656f15f5d48aee
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-09-03 09:53:34 +0200

    better indexes for SQLite + SELECT example as comment

--- ip2location.ipv4.sql
+++ ip2location.ipv4.sql
@@ -5,14 +5,16 @@ CREATE TABLE ip2location (
   ip_from INT(10),
   ip_to INT(10),
   country_code CHAR(2),
-  country_name VARCHAR(64)
+  country_name VARCHAR(64),
+  PRIMARY KEY(ip_from, ip_to)
 );
-
-CREATE INDEX idx_ip_from_to ON ip2location (ip_from, ip_to);
 
 -- https://lite.ip2location.com/database/ip-country
 .mode csv
 .import IP2LOCATION-LITE-DB1.CSV ip2location
 
 DELETE FROM ip2location WHERE country_code = "-";
+
+-- SELECT country_code AS code, country_name AS name FROM ip2location WHERE 35977539 BETWEEN ip_from AND ip_to;
+
 .exit
